717684ccb43e67afeebc399ed071df95d5a1cb22
Justin Yoo <justin.yoo@aliencube.com>
2013-06-11 12:28:15 +1000

    Added Web.UI project.

---
+++ SourceCodes/KoreanPostcodeAddressConverter.Web.UI/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aliencube.Utilities.KoreanPostcodeAddressConverter.Web.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
