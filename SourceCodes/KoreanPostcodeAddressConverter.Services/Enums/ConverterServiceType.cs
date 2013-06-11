d69af794861fb4092a02a93028a7b7c7b7588d13
Justin Yoo <justin.yoo@aliencube.com>
2013-06-12 09:10:01 +1000

    Added converter service type enums.

---
+++ SourceCodes/KoreanPostcodeAddressConverter.Services/Enums/ConverterServiceType.cs
@@ -0,0 +1,23 @@
+﻿namespace Aliencube.Utilities.KoreanPostcodeAddressConverter.Services.Enums
+{
+    /// <summary>
+    /// This specifies the type of the converter service.
+    /// </summary>
+    public enum ConverterServiceType
+    {
+        /// <summary>
+        /// This indicates that no converter service type is defined.
+        /// </summary>
+        Undefined = 0,
+
+        /// <summary>
+        /// This indicates that LOT-based address converter type is defined.
+        /// </summary>
+        Lot = 1,
+
+        /// <summary>
+        /// This indicates that street-based address converter type is defined.
+        /// </summary>
+        Street = 2
+    }
+}
