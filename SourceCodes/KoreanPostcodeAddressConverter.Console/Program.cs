8ac8cd20f7837dcb2a5efc18273e91fd196aec1f
Justin Yoo <justin.yoo@aliencube.com>
2013-06-14 12:40:20 +1000

    Added a new console app project.

---
+++ SourceCodes/KoreanPostcodeAddressConverter.Console/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Aliencube.Utilities.KoreanPostcodeAddressConverter.Services;
+using Aliencube.Utilities.KoreanPostcodeAddressConverter.Services.Enums;
+using Aliencube.Utilities.KoreanPostcodeAddressConverter.Services.Events;
+using log4net;
+
+namespace Aliencube.Utilities.KoreanPostcodeAddressConverter.Console
+{
+    /// <summary>
+    /// This represents the main entry point of the program entity.
+    /// </summary>
+    public class Program
+    {
+        private static readonly ILog _log = LogManager.GetLogger("KPAC Console");
+
+        #region Methods
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// <param name="args">List of arguments.</param>
+        public static void Main(string[] args)
+        {
+            ShowSplash();
+            try
+            {
+                ProcessRequests(args);
+                System.Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                if (_log.IsErrorEnabled)
+                {
+                    var sb = new StringBuilder();
+                    sb.AppendLine("ERROR:");
+                    sb.AppendLine();
+                    sb.AppendLine(ex.Message);
+                    sb.AppendLine();
+                    sb.AppendLine(ex.StackTrace);
+
+                    _log.Error(sb.ToString());
+                }
+
+                if (ex.GetType() != typeof(ArgumentException))
+                    return;
+
+                System.Console.WriteLine();
+                ShowUsage();
+            }
+        }
+
+        /// <summary>
+        /// Shows the splash message.
+        /// </summary>
+        private static void ShowSplash()
+        {
+            var fvi = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location);
+            var sb = new StringBuilder();
+            sb.AppendLine(String.Format("{0} v{1}", fvi.ProductName, fvi.FileVersion));
+            sb.AppendLine("------------------------------");
+
+            System.Console.WriteLine(sb.ToString());
+        }
+
+        /// <summary>
+        /// Shows the usage of the app message.
+        /// </summary>
+        private static void ShowUsage()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("Usage:");
+            sb.AppendLine("  KoreanPostcodeAddressUpdaterConsole.exe [Lot|Street]");
+            sb.AppendLine();
+            sb.AppendLine("Parameter:");
+            sb.AppendLine("  Lot:    Run LOT-based address converter service.");
+            sb.AppendLine("  Street: Run Street-based address converter service.");
+            sb.AppendLine();
+
+            System.Console.WriteLine(sb.ToString());
+        }
+
+        /// <summary>
+        /// Gets the converter service type.
+        /// </summary>
+        /// <param name="args">List of arguments.</param>
+        /// <returns>Returns the converter service type.</returns>
+        private static ConverterServiceType GetServiceType(IList<string> args)
+        {
+            var serviceType = ConverterServiceType.Undefined;
+
+            ConverterServiceType result;
+            if (Enum.TryParse(args[0], true, out result))
+                serviceType = result;
+
+            return serviceType;
+        }
+
+        /// <summary>
+        /// Processes the requests based on the argument provided.
+        /// </summary>
+        /// <param name="args">List of arguments.</param>
+        private static void ProcessRequests(IList<string> args)
+        {
+            if (args == null || !args.Any())
+                throw new ArgumentException("No argument found");
+
+            var serviceType = GetServiceType(args);
+            if (serviceType == ConverterServiceType.Undefined)
+                throw new ArgumentException("Invalid arguments");
+
+            var settings = Settings.Instance;
+
+            if (_log.IsInfoEnabled)
+                _log.Info(String.Format("Starting - {0}", Convert.ToString(serviceType)));
+
+            var factory = new UpdaterServiceFactory(settings);
+            factory.StatusChanged += Factory_StatusChanged;
+            factory.ProcessRequests(serviceType);
+
+            if (_log.IsInfoEnabled)
+                _log.Info(String.Format("Stopped - {0}", Convert.ToString(serviceType)));
+        }
+
+        /// <summary>
+        /// Occurs when status change event is raised.
+        /// </summary>
+        /// <param name="sender">Object that triggers the event.</param>
+        /// <param name="e">Provides data for the status change event.</param>
+        static void Factory_StatusChanged(object sender, StatusChangeEventArgs e)
+        {
+            if (_log.IsInfoEnabled)
+                _log.Info(e.StatusMessage);
+        }
+        #endregion
+    }
+}
---
+++ SourceCodes/KoreanPostcodeAddressConverter.Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Korean Postcode Address Converter Console Application")]
+[assembly: AssemblyDescription("This runs conversion jobs in the command prompt.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Aliencube Consulting")]
+[assembly: AssemblyProduct("KoreanPostcodeAddressConverter.Console")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("24f43e81-1fb9-418f-9bdf-a2a5ce4c32ce")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
