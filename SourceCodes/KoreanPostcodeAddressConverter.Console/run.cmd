2cfb1e1668c149e24bc028e17b460c3fae4937a2
Justin Yoo <justin.yoo@aliencube.com>
2013-06-14 15:39:55 +1000

    Added a batch file to run the converter for both LOT- and Street-based address.

---
+++ SourceCodes/KoreanPostcodeAddressConverter.Console/run.cmd
@@ -0,0 +1,8 @@
+﻿@echo off
+
+REM	Convert LOT-based postcode address.
+KoreanPostcodeAddressConverterConsole.exe Lot
+
+
+REM	Convert street-based postcode address.
+KoreanPostcodeAddressConverterConsole.exe Street
