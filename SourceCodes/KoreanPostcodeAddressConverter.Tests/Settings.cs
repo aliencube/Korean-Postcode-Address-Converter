d29198e9682ab1e248fff5364760d6b70bfdcd1e
Justin Yoo <justin.yoo@aliencube.com>
2013-06-18 09:51:32 +1000

    Added Settings classes for each service inherited from the base service settings.

---
+++ SourceCodes/KoreanPostcodeAddressConverter.Tests/Settings.cs
@@ -0,0 +1,33 @@
+﻿namespace Aliencube.Utilities.KoreanPostcodeAddressConverter.Tests
+{
+    /// <summary>
+    /// This represents the configuration settings entity.
+    /// </summary>
+    public class Settings : Aliencube.Utilities.KoreanPostcodeAddressConverter.Services.Settings
+    {
+        #region Constructors
+        /// <summary>
+        ///	Initialises a new instance of the Settings object as private.
+        /// </summary>
+        private Settings()
+        {
+        }
+        #endregion
+
+        #region Properties
+        private static Settings _instance;
+        /// <summary>
+        /// Gets the instance of the settings object.
+        /// </summary>
+        public static Settings Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Settings();
+                return _instance;
+            }
+        }
+        #endregion
+    }
+}
